1a2be4d53725ed3554eeb83b882ade3532d35fa0
Eckart Cobo-Briesewitz <68149355+Tupryk@users.noreply.github.com>
2024-11-20 18:00:41 +0100

    unet added

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -172,3 +172,111 @@ class VAE(AE):
         pred_images = np.minimum(pred_images, 1)
         pred_images = np.transpose(pred_images, (0, 2, 3, 1))
         return pred_images
+
+
+class DoubleConv(nn.Module):
+    """
+    Two consecutive convolutional layers with BatchNorm and ReLU.
+    """
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNetAutoencoder(nn.Module):
+    def __init__(self, latent_dim=64):
+        super(UNetAutoencoder, self).__init__()
+
+        # Encoder path
+        self.enc1 = DoubleConv(3, 64)
+        self.enc2 = DoubleConv(64, 128)
+        self.enc3 = DoubleConv(128, 256)
+        self.enc4 = DoubleConv(256, 512)
+        self.enc5 = DoubleConv(512, 1024)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layer for encoding
+        self.fc_encode = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * 8 * 8, latent_dim),
+            nn.ReLU(inplace=True)
+        )
+
+        # Fully connected layer for decoding
+        self.fc_decode = nn.Sequential(
+            nn.Linear(latent_dim, 1024 * 8 * 8),
+            nn.ReLU(inplace=True)
+        )
+
+        # Decoder path
+        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec4 = DoubleConv(1024, 512)
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        # Output layer
+        self.out_conv = nn.Conv2d(64, 3, kernel_size=1)
+
+    def encode(self, x):
+        # Encoder
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+        enc5 = self.enc5(self.pool(enc4))
+
+        # Flatten and encode to latent vector
+        encoded = self.fc_encode(enc5)
+        return encoded
+
+    def forward(self, x):
+        # Encoder
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+        enc5 = self.enc5(self.pool(enc4))
+
+        # Flatten and encode to latent vector
+        encoded = self.fc_encode(enc5)
+
+        # Decode latent vector back to spatial features
+        decoded = self.fc_decode(encoded)
+        decoded = decoded.view(-1, 1024, 8, 8)  # Reshape back to feature map size
+
+        # Decoder
+        dec4 = self.upconv4(decoded)
+        dec4 = self.dec4(torch.cat([dec4, enc4], dim=1))
+        dec3 = self.upconv3(dec4)
+        dec3 = self.dec3(torch.cat([dec3, enc3], dim=1))
+        dec2 = self.upconv2(dec3)
+        dec2 = self.dec2(torch.cat([dec2, enc2], dim=1))
+        dec1 = self.upconv1(dec2)
+        dec1 = self.dec1(torch.cat([dec1, enc1], dim=1))
+
+        # Output
+        return self.out_conv(dec1)
+    
+    def forward_clean(self, original_images):
+        # This should be done with the dataloader stuff maybe
+        pred_images = self.forward(original_images)
+        pred_images = pred_images.detach().cpu().numpy()
+        pred_images = np.maximum(pred_images, 0)
+        pred_images = np.minimum(pred_images, 1)
+        pred_images = np.transpose(pred_images, (0, 2, 3, 1))
+        return pred_images
